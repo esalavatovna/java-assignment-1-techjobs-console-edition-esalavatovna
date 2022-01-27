e790ffad4012ccead7fd0e0d5e4303879f7ca46a
eleonorabogdanova <e.salavatovna@gmail.com>
2022-01-26 19:57:10 -0600

    first commmit

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -118,8 +118,24 @@ public class TechJobs {
     }
 
     // Print a list of jobs
+    // I DID
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if(someJobs.isEmpty()){
+            System.out.print("No Results");
+        } else{
+            for(HashMap<String,String> job : someJobs){
+                String jobInfo = "\n*****\n";
+                for (Map.Entry<String, String> jobColumn : job.entrySet()) {
 
-        System.out.println("printJobs is not implemented yet");
+                    jobInfo += jobColumn.getKey() + ": " + jobColumn.getValue() + "\n";
+
+                }
+
+                jobInfo+="*****";
+                System.out.println(jobInfo);
+            }
+        }
+
+
     }
 }
